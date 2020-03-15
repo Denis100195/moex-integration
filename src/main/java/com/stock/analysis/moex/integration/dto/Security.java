0fe9963b8e04fb414f45b831d095147a47a8dd74
Denis100195 <denfil1995@gmail.com>
2020-03-15 19:17:38 +0300

    initial commit

---
+++ src/main/java/com/stock/analysis/moex/integration/dto/Security.java
@@ -0,0 +1,15 @@
+package com.stock.analysis.moex.integration.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Security {
+
+
+}
---
+++ src/main/java/com/stock/analysis/moex/integration/service/MoexDataService.java
@@ -0,0 +1,17 @@
+package com.stock.analysis.moex.integration.service;
+
+import com.stock.analysis.moex.integration.dto.Security;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Service
+public class MoexDataService {
+
+    public List<Security> parseDoc(LocalDate date){
+
+    }
+
+}
