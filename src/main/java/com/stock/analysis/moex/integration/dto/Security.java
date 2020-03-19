a2017da5526ff4b747cd57357d3dec6527aa0008
Denis100195 <denfil1995@gmail.com>
2020-03-19 20:46:57 +0300

    parser for moex

--- src/main/java/com/stock/analysis/moex/integration/dto/Security.java
+++ src/main/java/com/stock/analysis/moex/integration/dto/Security.java
@@ -5,11 +5,190 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Security {
+    private String boardId;
+    private LocalDate tradeDate;
+    private String shortName;
+    private String secId;
+    private double numTrades;
+    private double value;
+    private double open;
+    private double low;
+    private double hight;
+    private double legalClosePrice;
+    private double waPrice;
+    private double close;
+    private double volume;
+    private double marketPrice2, MarketPrice3;
+    private double admittedQoute;
+    private double mp2ValTrd;
+    private double marketPrice3TradeValue;
+    private double admittedValue;
+    private double waval;
 
+    public String getBoardId() {
+        return boardId;
+    }
 
+    public void setBoardId(String boardId) {
+        this.boardId = boardId;
+    }
+
+    public LocalDate getTradeDate() {
+        return tradeDate;
+    }
+
+    public void setTradeDate(LocalDate tradeDate) {
+        this.tradeDate = tradeDate;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getSecId() {
+        return secId;
+    }
+
+    public void setSecId(String secId) {
+        this.secId = secId;
+    }
+
+    public double getNumTrades() {
+        return numTrades;
+    }
+
+    public void setNumTrades(double numTrades) {
+        this.numTrades = numTrades;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getOpen() {
+        return open;
+    }
+
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public double getHight() {
+        return hight;
+    }
+
+    public void setHight(double hight) {
+        this.hight = hight;
+    }
+
+    public double getLegalClosePrice() {
+        return legalClosePrice;
+    }
+
+    public void setLegalClosePrice(double legalClosePrice) {
+        this.legalClosePrice = legalClosePrice;
+    }
+
+    public double getWaPrice() {
+        return waPrice;
+    }
+
+    public void setWaPrice(double waPrice) {
+        this.waPrice = waPrice;
+    }
+
+    public double getClose() {
+        return close;
+    }
+
+    public void setClose(double close) {
+        this.close = close;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public double getMarketPrice2() {
+        return marketPrice2;
+    }
+
+    public void setMarketPrice2(double marketPrice2) {
+        this.marketPrice2 = marketPrice2;
+    }
+
+    public double GetMarketPrice3() {
+        return MarketPrice3;
+    }
+
+    public void setMarketPrice3(double MarketPrice3) {
+        this.MarketPrice3 = MarketPrice3;
+    }
+
+    public double getAdmittedQoute() {
+        return admittedQoute;
+    }
+
+    public void setAdmittedQoute(double admittedQoute) {
+        this.admittedQoute = admittedQoute;
+    }
+
+    public double getMp2ValTrd() {
+        return mp2ValTrd;
+    }
+
+    public void setMp2ValTrd(double mp2ValTrd) {
+        this.mp2ValTrd = mp2ValTrd;
+    }
+
+    public double getMarketPrice3TradeValue() {
+        return marketPrice3TradeValue;
+    }
+
+    public void setMarketPrice3TradeValue(double marketPrice3TradeValue) {
+        this.marketPrice3TradeValue = marketPrice3TradeValue;
+    }
+
+    public double getAdmittedValue() {
+        return admittedValue;
+    }
+
+    public void setAdmittedValue(double admittedValue) {
+        this.admittedValue = admittedValue;
+    }
+
+    public double getWaval() {
+        return waval;
+    }
+
+    public void setWaval(double waval) {
+        this.waval = waval;
+    }
 }
--- src/main/java/com/stock/analysis/moex/integration/service/MoexDataService.java
+++ src/main/java/com/stock/analysis/moex/integration/service/MoexDataService.java
@@ -3,15 +3,81 @@ package com.stock.analysis.moex.integration.service;
 import com.stock.analysis.moex.integration.dto.Security;
 import org.springframework.stereotype.Service;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 
 @Service
 public class MoexDataService {
 
-    public List<Security> parseDoc(LocalDate date){
+    public List<Security> parseDoc(LocalDate date) throws Exception {
+        InputStream stream =
+                URI.create("http://iss.moex.com/iss/history/engines/stock/markets/shares/boards/tqbr/securities.xml")
+                .toURL().openStream();
+        List<Security> securityList = null;
+        Security currSec = null;
+        String tagContent = null;
 
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        XMLStreamReader reader = factory.createXMLStreamReader(stream);
+
+        while (reader.hasNext()){
+            int event = reader.next();
+
+            switch (event){
+                case XMLStreamConstants.START_ELEMENT:
+                    if ("rows".equals(reader.getLocalName())){
+                        securityList = new ArrayList<>();
+                    }
+                    if ("row".equals(reader.getLocalName())){
+                        currSec = new Security();
+                        currSec.setBoardId(reader.getAttributeValue(0));
+                        currSec.setTradeDate(LocalDate.parse(reader.getAttributeValue(1)));
+                        currSec.setShortName(reader.getAttributeValue(2));
+                        currSec.setSecId(reader.getAttributeValue(3));
+                        currSec.setNumTrades(Double.parseDouble(reader.getAttributeValue(4)));
+                        currSec.setValue(Double.parseDouble(reader.getAttributeValue(5)));
+                        currSec.setOpen(Double.parseDouble(reader.getAttributeValue(6)));
+                        currSec.setLow(Double.parseDouble(reader.getAttributeValue(7)));
+                        currSec.setHight(Double.parseDouble(reader.getAttributeValue(8)));
+                        currSec.setLegalClosePrice(Double.parseDouble(reader.getAttributeValue(9)));
+                        currSec.setWaPrice(Double.parseDouble(reader.getAttributeValue(10)));
+                        currSec.setClose(Double.parseDouble(reader.getAttributeValue(11)));
+                        currSec.setVolume(Double.parseDouble(reader.getAttributeValue(12)));
+                        currSec.setMarketPrice2(Double.parseDouble(reader.getAttributeValue(13)));
+                        currSec.setMarketPrice3(Double.parseDouble(reader.getAttributeValue(14)));
+                        currSec.setAdmittedQoute(Double.parseDouble(reader.getAttributeValue(15)));
+                        currSec.setMp2ValTrd(Double.parseDouble(reader.getAttributeValue(16)));
+                        currSec.setMarketPrice3TradeValue(Double.parseDouble(reader.getAttributeValue(17)));
+                        currSec.setAdmittedValue(Double.parseDouble(reader.getAttributeValue(18)));
+                        currSec.setWaval(Double.parseDouble(reader.getAttributeValue(19)));
+
+                    }
+                    break;
+                case XMLStreamConstants.CHARACTERS:
+                    tagContent = reader.getText().trim();
+                    break;
+
+
+                case XMLStreamConstants.START_DOCUMENT:
+                    securityList = new ArrayList<>();
+                    break;
+
+            }
+        }
+        for (Security security: securityList){
+            System.out.println(security);
+        }
+        return securityList;
     }
+
 
 }
