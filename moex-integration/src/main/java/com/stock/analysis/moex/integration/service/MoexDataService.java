2586ea7fec87a32a012d9527abfdbf925433457e
Denis100195 <denfil1995@gmail.com>
2020-03-23 21:29:26 +0300

    test

--- moex-integration/src/main/java/com/stock/analysis/moex/integration/service/MoexDataService.java
+++ moex-integration/src/main/java/com/stock/analysis/moex/integration/service/MoexDataService.java
@@ -2,12 +2,14 @@ package com.stock.analysis.moex.integration.service;
 
 import com.stock.analysis.moex.integration.dto.Security;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.stereotype.Service;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamReader;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.net.URI;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -16,18 +18,16 @@ import java.util.List;
 // TODO над классом навесить аннотацию @Slf4j - это тоже lombok,
 //  позволяет избавиться от конструкции Logger logger = Logger.getLogger(MoexDataService.class);
 // поищи уроки про логгирование в java, про slf4j и logback
-@Service
-public class MoexDataService {
 
+@Service
+@Slf4j
+public class MoexDataService {
     public List<Security> parseDoc(LocalDate date) throws Exception {
-        // TODO url давай с датой вернем, нужно на определенную дату данные получать, в конце ?date=2020-03-17
-        //  ну и передавать туда date.toString() -
         InputStream stream =
-                URI.create("http://iss.moex.com/iss/history/engines/stock/markets/shares/boards/tqbr/securities.xml")
+                URI.create("http://iss.moex.com/iss/history/engines/stock/markets/shares/boards/tqbr/securities.xml?date=" + date.toString())
                         .toURL().openStream();
-        List<Security> securityList = null;
-        Security currSec = null;
-        String tagContent = null;
+        List<Security> securityList = new ArrayList<>();
+
 
         XMLInputFactory factory = XMLInputFactory.newInstance();
         XMLStreamReader reader = factory.createXMLStreamReader(stream);
@@ -35,48 +35,38 @@ public class MoexDataService {
         while (reader.hasNext()) {
             int event = reader.next();
 
-            switch (event) {
-                case XMLStreamConstants.START_ELEMENT:
-                    if ("rows".equals(reader.getLocalName())) {
-                        securityList = new ArrayList<>();
-                    }
-                    if ("row".equals(reader.getLocalName())) {
-                        // TODO тут лучше использовать builder
-                        currSec = new Security();
-                        currSec.setBoardId(reader.getAttributeValue(0));
-                        currSec.setTradeDate(LocalDate.parse(reader.getAttributeValue(1)));
-                        currSec.setShortName(reader.getAttributeValue(2));
-                        currSec.setSecId(reader.getAttributeValue(3));
-                        currSec.setNumTrades(Double.parseDouble(reader.getAttributeValue(4)));
-                        currSec.setValue(Double.parseDouble(reader.getAttributeValue(5)));
-                        currSec.setOpen(Double.parseDouble(reader.getAttributeValue(6)));
-                        currSec.setLow(Double.parseDouble(reader.getAttributeValue(7)));
-                        currSec.setHight(Double.parseDouble(reader.getAttributeValue(8)));
-                        currSec.setLegalClosePrice(Double.parseDouble(reader.getAttributeValue(9)));
-                        currSec.setWaPrice(Double.parseDouble(reader.getAttributeValue(10)));
-                        currSec.setClose(Double.parseDouble(reader.getAttributeValue(11)));
-                        currSec.setVolume(Double.parseDouble(reader.getAttributeValue(12)));
-                        currSec.setMarketPrice2(Double.parseDouble(reader.getAttributeValue(13)));
-                        currSec.setMarketPrice3(Double.parseDouble(reader.getAttributeValue(14)));
-                        currSec.setAdmittedQoute(Double.parseDouble(reader.getAttributeValue(15)));
-                        currSec.setMp2ValTrd(Double.parseDouble(reader.getAttributeValue(16)));
-                        currSec.setMarketPrice3TradeValue(Double.parseDouble(reader.getAttributeValue(17)));
-                        currSec.setAdmittedValue(Double.parseDouble(reader.getAttributeValue(18)));
-                        currSec.setWaval(Double.parseDouble(reader.getAttributeValue(19)));
-                    }
-                    break;
-                case XMLStreamConstants.CHARACTERS:
-                    tagContent = reader.getText().trim();
-                    break;
+            if (event == XMLStreamConstants.START_ELEMENT && "row".equals(reader.getLocalName())) {
 
-                case XMLStreamConstants.START_DOCUMENT:
-                    securityList = new ArrayList<>();
-                    break;
+                Security currSec = Security.builder()
+                        .boardId(reader.getAttributeValue(0))
+                        .tradeDate(LocalDate.parse(reader.getAttributeValue(1)))
+                        .shortName(reader.getAttributeValue(2))
+                        .secId(reader.getAttributeValue(3))
+                        .numTrades(NumberUtils.isCreatable(reader.getAttributeValue(4)) ? new BigDecimal(reader.getAttributeValue(4)) : null)
+                        .value(NumberUtils.isCreatable(reader.getAttributeValue(5)) ? new BigDecimal(reader.getAttributeValue(5)) : null)
+                        .open(NumberUtils.isCreatable(reader.getAttributeValue(6)) ? new BigDecimal(reader.getAttributeValue(6)) : null)
+                        .low(NumberUtils.isCreatable(reader.getAttributeValue(7)) ? new BigDecimal(reader.getAttributeValue(7)) : null)
+                        .high(NumberUtils.isCreatable(reader.getAttributeValue(8)) ? new BigDecimal(reader.getAttributeValue(8)) : null)
+                        .legalClosePrice(NumberUtils.isCreatable(reader.getAttributeValue(9)) ? new BigDecimal(reader.getAttributeValue(9)) : null)
+                        .waPrice(NumberUtils.isCreatable(reader.getAttributeValue(10)) ? new BigDecimal(reader.getAttributeValue(10)) : null)
+                        .close(NumberUtils.isCreatable(reader.getAttributeValue(11)) ? new BigDecimal(reader.getAttributeValue(11)) : null)
+                        .volume(NumberUtils.isCreatable(reader.getAttributeValue(12)) ? new BigDecimal(reader.getAttributeValue(12)) : null)
+                        .marketPrice2(NumberUtils.isCreatable(reader.getAttributeValue(13)) ? new BigDecimal(reader.getAttributeValue(13)) : null)
+                        .marketPrice3(NumberUtils.isCreatable(reader.getAttributeValue(14)) ? new BigDecimal(reader.getAttributeValue(14)) : null)
+                        .admittedQoute(NumberUtils.isCreatable(reader.getAttributeValue(15)) ? new BigDecimal(reader.getAttributeValue(15)) : null)
+                        .mp2ValTrd(NumberUtils.isCreatable(reader.getAttributeValue(16)) ? new BigDecimal(reader.getAttributeValue(16)) : null)
+                        .marketPrice3TradeValue(NumberUtils.isCreatable(reader.getAttributeValue(17)) ? new BigDecimal(reader.getAttributeValue(17)) : null)
+                        .admittedValue(NumberUtils.isCreatable(reader.getAttributeValue(18)) ? new BigDecimal(reader.getAttributeValue(18)) : null)
+                        .waval(NumberUtils.isCreatable(reader.getAttributeValue(19)) ? new BigDecimal(reader.getAttributeValue(19)) : null)
+                        .build();
+
+                securityList.add(currSec);
+            }
+            if (event == XMLStreamConstants.END_ELEMENT && "rows".equals(reader.getLocalName())){
+                return securityList;
             }
         }
-        for (Security security : securityList) {
-            System.out.println(security);
-        }
+        securityList.forEach(System.out::println);
         return securityList;
     }
 
---
+++ moex-integration/src/test/java/com/stock/analysis/moex/integration/service/MoexDataServiceTest.java
@@ -0,0 +1,20 @@
+package com.stock.analysis.moex.integration.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class MoexDataServiceTest {
+    @Autowired
+    private MoexDataService moexDataService;
+    @org.junit.Test
+    public void testParseDoc() throws Exception {
+        moexDataService.parseDoc(LocalDate.of(2020,3,20));
+    }
+}
