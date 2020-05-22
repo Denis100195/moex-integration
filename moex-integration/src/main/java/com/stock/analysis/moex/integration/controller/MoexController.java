817bf9bfa92d70c60f4fc53a75f86d93dfeede5c
Denis100195 <denfil1995@gmail.com>
2020-05-22 15:05:33 +0300

    add controller

--- moex-integration/src/main/java/com/stock/analysis/moex/integration/controller/MoexController.java
+++ moex-integration/src/main/java/com/stock/analysis/moex/integration/controller/MoexController.java
@@ -1,4 +1,31 @@
 package com.stock.analysis.moex.integration.controller;
 
+import com.stock.analysis.moex.integration.dto.Security;
+import com.stock.analysis.moex.integration.service.MoexDataService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
 public class MoexController {
+
+    private final MoexDataService moexDataService;
+
+    public MoexController(MoexDataService moexDataService){
+        this.moexDataService = moexDataService;
+    }
+
+    @GetMapping(value = "/getAllSecOnDate")
+    public List<Security> getAllSecOnDate (@RequestParam(value = "date")LocalDate date){
+        return moexDataService.getSecurityDataOnDate(date);
+    }
+
+    @GetMapping(value = "/getOneSecByNameOnDate")
+    public Security getOneSecByNameOnDate(@RequestParam(value = "date")LocalDate date,
+                                          @RequestParam(value = "secName")String secName){
+        return moexDataService.getOneSecurityByNameOnDate(date, secName);
+    }
 }
